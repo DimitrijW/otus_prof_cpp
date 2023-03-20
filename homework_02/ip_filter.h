bc213d8592b7f4705324db62b99233171bbc5b10
DimitrijW <dimitrij.code@gmail.com>
2023-03-20 13:59:58 +0100

    definitions in header file

---
+++ homework_02/ip_filter.h
@@ -0,0 +1,12 @@
+#ifndef _IPFILTER_H_
+#define _IPFILTER_H_
+
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::vector<std::string> split(const std::string &str, char d);
+
+#endif
